41e7677fa52098c4c2d05c31ffc74b1b2c62442a
Robert <robertjazzsax@gmail.com>
2023-09-28 09:23:59 +0100

    Se crea repositorio MasterClass Java

---
+++ src/FirstClass.java
@@ -0,0 +1,6 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        String regards = "Hello World baby";
+        System.out.println("regards = " + regards.toUpperCase());
+    }
+}
